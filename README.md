32162e3e3a924a5ed9eca1183295f9757d99332e
薛翔 <xuexiangjys@163.com>
2018-06-26 22:02:00 +0800

    更新代码

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # architect-java
 成为架构师必须要看的算法实例
+
+
+
+
+
+
+
+
+
+
+
+
+## 特别感谢
+https://github.com/xingshaocheng/architect-awesome
---
+++ src/algorithm/sort/insert/BinaryInsertSort.java
@@ -0,0 +1,8 @@
+package algorithm.sort.insert;
+
+/**
+ * 二分法插入排序（按二分法找到合适位置插入）
+ * 二分法插入排序的思想和直接插入一样，只是找合适的插入位置的方式不同，这里是按二分法找到合适的位置，可以减少比较的次数。
+ */
+public class BinaryInsertSort {
+}
